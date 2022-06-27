cb7b7bfdae7d90c46851f444a1b9906ab142a1de
ktyoung <ktyoung0507@gmail.com>
2022-06-27 11:54:19 +0900

    charExample

--- chap02/src/sec01/exam01_byte/ByteExample.java
+++ chap02/src/sec01/exam01_byte/ByteExample.java
@@ -5,7 +5,7 @@ public class ByteExample {
 	public static void main(String[] args) {
 		byte var1 = -128;
 		byte var2 = -30;
-		byte var3 = -0;
+		byte var3 = 0;
 		byte var4 = 30;
 		byte var5 = 127;
 		// byte var6 = 128; byteÇü : -128 ~ 127 ¹üÀ§
