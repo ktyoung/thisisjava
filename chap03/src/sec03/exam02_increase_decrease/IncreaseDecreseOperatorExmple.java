3e0d304abb002eeb4f90a4261b1dbc87669f6fa7
ktyoung <ktyoung0507@gmail.com>
2022-06-28 13:00:47 +0900

    DenyLogicOperatorExample

--- chap03/src/sec03/exam02_increase_decrease/IncreaseDecreseOperatorExmple.java
+++ chap03/src/sec03/exam02_increase_decrease/IncreaseDecreseOperatorExmple.java
@@ -23,12 +23,12 @@ public class IncreaseDecreseOperatorExmple {
 		System.out.println("x = " + x);
 		
 		System.out.println("----------");
-		z = ++x; // z = 14, x=14
+		z = ++x; // z = 14, x = 14
 		System.out.println("z = " + z);
 		System.out.println("x = " + x);
 		
 		System.out.println("----------");
-		z = ++x + y++; // z = 23, x = 15, y = 9
+		z = ++x + y++; // z = 23, x = 15, y = 8
 		System.out.println("z = " + z);
 		System.out.println("x = " + x);
 		System.out.println("y = " + y);
