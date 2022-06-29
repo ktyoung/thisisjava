ba33938852d933fde1d0a6f423682c6294178fd3
ktyoung <ktyoung0507@gmail.com>
2022-06-29 13:00:07 +0900

    AssignmentOperatorExample

---
+++ chap03/src/sec04/exam06_assignment/AssignmentOperatorExample.java
@@ -0,0 +1,28 @@
+package sec04.exam06_assignment;
+
+public class AssignmentOperatorExample {
+
+	public static void main(String[] args) {
+		int result = 0;
+		// result = result + 10
+		result += 10;
+		System.out.println("result = " + result);
+
+		// result = result - 5
+		result -= 5;
+		System.out.println("result = " + result);
+		
+		// result = result * 3
+		result *= 3;
+		System.out.println("result = " + result);
+		
+		// result = result / 5
+		result /= 5;
+		System.out.println("result = " + result);
+		
+		// result = result % 3
+		result %= 3;
+		System.out.println("result = " + result);
+	}
+
+}
