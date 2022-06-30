314ab2508c1caa831181c37ef55dc5f61775b588
ktyoung <ktyoung0507@gmail.com>
2022-06-30 12:25:58 +0900

    WhileKeyControlExample

---
+++ chap04/src/sec03/exam01_for/ForSumFrom1To10.java
@@ -0,0 +1,16 @@
+package sec03.exam01_for;
+
+public class ForSumFrom1To10 {
+
+	public static void main(String[] args) {
+		int i = 0;
+		int sum = 0;
+		
+		for(i=0; i<=10; i++) {
+			sum += i;
+			System.out.println(i + "¹øÂ° ÇÕ : " + sum);
+		}
+
+	}
+
+}
