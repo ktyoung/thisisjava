ed0a47463b0d8a00ca71f15d879e67a2f10e8bd6
ktyoung <ktyoung0507@gmail.com>
2022-07-01 10:34:33 +0900

    ForMultiplicationTable

--- chap04/src/sec00/exam00_ForMultiplicationTable/ForMultiplicationTable.java
+++ chap04/src/sec00/exam00_ForMultiplicationTable/ForMultiplicationTable.java
@@ -3,16 +3,16 @@ package sec00.exam00_ForMultiplicationTable;
 public class ForMultiplicationTable {
 
 	public static void main(String[] args) {
+		for(int k=2; k<=9; k++) {
+			System.out.print("[" + k + "]´Ü" + "\t\t");
+		}
+		System.out.println(" ");
+		
 		for(int j=1; j<=9; j++) {
-			for(int i=2; i<=9; i++) {
+			for(int i=2; i<=9; i++) {		
 				System.out.print(i + " x " + j + " = " + (i*j) + "\t");
 			}
 			System.out.println(" ");
 		}
-
 	}
-
-
 }
-
-
