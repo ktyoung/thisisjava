83eff6e0c98b99c1ac706aa2a86846c0889a68de
ktyoung <ktyoung0507@gmail.com>
2022-07-05 10:10:22 +0900

    ArrayReferenceObjectExample

---
+++ backjoon/src/backjoon/Backjoon10430.java
@@ -0,0 +1,25 @@
+package backjoon;
+
+import java.util.Scanner;
+
+public class Backjoon10430 {
+
+	public static void main(String[] args) {
+		int A = 0;
+		int B = 0;
+		int C = 0;
+		
+		Scanner sc = new Scanner (System.in);
+		A = sc.nextInt();
+		B = sc.nextInt();
+		C = sc.nextInt();
+		
+		System.out.println( (A+B)%C );
+		System.out.println( ((A%C)+(B%C))%C );
+		System.out.println( (A*B)%C );
+		System.out.println( ((A%C)*(B%C))%C );
+		
+
+	}
+
+}
--- backjoon/src/backjoon/Backjoon18108.java
+++ backjoon/src/backjoon/Backjoon18108.java
@@ -1,8 +1,17 @@
 package backjoon;
 
+import java.util.Scanner;
+
 public class Backjoon18108 {
 
 	public static void main(String[] args) {
+		int year = 0;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		year = sc.nextInt();
+		System.out.println(year-544+1);
+		
 		
 
 	}
