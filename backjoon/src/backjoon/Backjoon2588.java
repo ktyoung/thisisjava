f3205a0b9f8223c8fe061ba3f8d6c1f802b2f191
ktyoung <ktyoung0507@gmail.com>
2022-07-05 10:27:22 +0900

    ArrayCopyByForExample

---
+++ backjoon/src/backjoon/Backjoon2588.java
@@ -0,0 +1,22 @@
+package backjoon;
+
+import java.util.Scanner;
+
+public class Backjoon2588 {
+
+	public static void main(String[] args) {
+		int A = 0;
+		int B = 0;
+		int num1 = 0;
+		int num2 = 0;
+		int num3 = 0;
+		
+		Scanner sc = new Scanner(System.in);
+		A = sc.nextInt();
+		B = sc.nextInt();
+		
+		System.out.println( A*B );
+		
+	}
+
+}
