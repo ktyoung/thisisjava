f3d734eea833bd2621adf0dc0a05e9e5588a9e73
ktyoung <ktyoung0507@gmail.com>
2022-07-06 11:58:25 +0900

    OOP

---
+++ chap06/src/sec04/exam01_class_new/Student.java
@@ -0,0 +1,7 @@
+package sec04.exam01_class_new;
+
+public class Student {
+	// 라이브러리 함수
+	
+
+}
---
+++ chap06/src/sec06/exam01_field_declaration/Car.java
@@ -0,0 +1,12 @@
+package sec06.exam01_field_declaration;
+
+public class Car {
+	// 라이브러리 함수
+	
+	// 필드 목록
+	String company = "현대자동차";
+	String model = "그랜저";
+	String color = "검정";
+	int maxSpeed = 350;
+	int speed;
+}
