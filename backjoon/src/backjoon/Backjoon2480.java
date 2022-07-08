438f3f2f58051dd752fbef0b26b20b9c36e68add
ktyoung <ktyoung0507@gmail.com>
2022-07-08 09:46:17 +0900

    Method Declaration

--- backjoon/src/backjoon/Backjoon2480.java
+++ backjoon/src/backjoon/Backjoon2480.java
@@ -3,20 +3,28 @@ package backjoon;
 import java.util.Scanner;
 
 public class Backjoon2480 {
-
-	public static void main(String[] args) {
-		int dice1, dice2, dice3, money = 0;
-		
-		Scanner sc = new Scanner(System.in);
-		
-		dice1 = sc.nextInt();
-		dice2 = sc.nextInt();
-		dice3 = sc.nextInt();
-		
-		if() {
-			
-		}
-
-	}
-
+    public static void main(String[] args) {
+    	
+        Scanner sc = new Scanner(System.in);
+        
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        
+        if(a == b && b == c) {
+            System.out.print(10000 + a*1000);
+        }
+        
+        else if(a == b || a == c) {
+            System.out.print(1000 + a*100);
+        }
+        
+        else if(b == c) {
+            System.out.print(1000 + b*100);
+        }
+        
+        else {
+            System.out.print((Math.max(Math.max(a, b), c)*100));
+        }
+    }
 }
