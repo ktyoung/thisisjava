3e1c971f128148d47fd4c4332ec5e76bcaf83fd6
ktyoung <ktyoung0507@gmail.com>
2022-07-19 11:10:31 +0900

    Casting

---
+++ chap08/src/sec05/exam04_casting/VehicleExample.java
@@ -0,0 +1,16 @@
+package sec05.exam04_casting;
+
+public class VehicleExample {
+
+	public static void main(String[] args) {
+		// 자동 타입 변환
+		Vehicle vehicle = new Bus();
+		vehicle.run();
+		// vehicle.checkFare();		// 부모 클래스에 선언된 메서드가 아니므로 컴파일 에러 → 강제 타입 변환 필요
+		
+		Bus bus = (Bus) vehicle;
+		bus.run();// 강제 형변환
+		bus.checkFare();
+	}
+
+}
