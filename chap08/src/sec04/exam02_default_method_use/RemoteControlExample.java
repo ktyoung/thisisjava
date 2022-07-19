38c77a1e9e5f47a2dea7c0c1b611e02cc181d38c
ktyoung <ktyoung0507@gmail.com>
2022-07-19 09:46:59 +0900

    Abstract Method Use

--- chap08/src/sec04/exam02_default_method_use/RemoteControlExample.java
+++ chap08/src/sec04/exam02_default_method_use/RemoteControlExample.java
@@ -1,4 +1,4 @@
-package sec04.exam01_abstract_method_use;
+package sec04.exam02_default_method_use;
 
 public class RemoteControlExample {
 
@@ -8,10 +8,16 @@ public class RemoteControlExample {
 		rc = new Television();		// rc에 Televison 클래스를 생성하면 Televison에 구현된 메서드가 동작
 		rc.turnOn();
 		rc.turnOff();
+		rc.setVolume(10);
+		rc.setMute(true);
+		rc.setMute(false);
 		
 		rc = new Audio();			// rc에 Audio 클래스를 생성하면 Audio 구현된 메서드가 동작
 		rc.turnOn();
 		rc.turnOff();
+		rc.setVolume(5);
+		rc.setMute(true);
+		rc.setMute(false);
 	}
 
 }
