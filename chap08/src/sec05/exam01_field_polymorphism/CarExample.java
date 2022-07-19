c7c9c2c70de79b7f7d96ef60d50c0bf983afab83
ktyoung <ktyoung0507@gmail.com>
2022-07-19 10:24:16 +0900

    Field Polymorphism

--- chap08/src/sec05/exam01_field_polymorphism/CarExample.java
+++ chap08/src/sec05/exam01_field_polymorphism/CarExample.java
@@ -3,9 +3,15 @@ package sec05.exam01_field_polymorphism;
 public class CarExample {
 
 	public static void main(String[] args) {
-		Car car = new Car();
+		Car myCar = new Car();
+		myCar.run(); // 4가지 위치 타이어 모두 한국타이어
+		System.out.println();
 		
-
+		// 앞쪽 타이어 금호 타이어로 교체
+		myCar.frontLeftTire = new KumhoTire();
+		myCar.frontRightTire = new KumhoTire();
+		
+		myCar.run();
 	}
 
 }
