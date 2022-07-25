0fb3d4ba3c37747d034b7c5a435e16f652d00aaa
ktyoung <ktyoung0507@gmail.com>
2022-07-25 10:52:43 +0900

    Nested Class

--- chap09/src/sec02/exam01_nestedclass_object/A.java
+++ chap09/src/sec02/exam01_nestedclass_object/A.java
@@ -10,4 +10,25 @@ public class A {
 		void method1() {}
 		// static void method2() {}
 	}
+	
+	static class C {
+		C() { System.out.println("C 按眉啊 积己凳"); }
+		int field1;
+		static int field2;
+		void method1() {}
+		static void method2() {}
+	}
+	
+	void method() {
+		class D {
+			D() { System.out.println("D 按眉啊 积己凳"); }
+			int field1;
+			// static int field2;
+			void method1() {}
+			// static void method2() {}
+		}
+		D d = new D();
+		d.field1 = 3;
+		d.method1();
+	}
 }
--- chap09/src/sec02/exam01_nestedclass_object/Main.java
+++ chap09/src/sec02/exam01_nestedclass_object/Main.java
@@ -3,18 +3,20 @@ package sec02.exam01_nestedclass_object;
 public class Main {
 
 	public static void main(String[] args) {
-		A a = new A();		// 인스턴스 멤버 클래스 객체 생성을 해주어야 함
+		A a = new A();		// 인스턴스 멤버 클래스 객체 생성을 해주어야 클래스 사용 가능
 		A.B b = a.new B(); 
 		b.field1 = 3;
 		b.method1();
-
+		
+		A.C c = new A.C();
+		c.field1 = 3;		// 스태틱 멤버 클래스 객체 생성을 하지 않아도 클래스 사용 가능
+		c.method1();
+		A.C.field2 = 3;
+		A.C.method2();
+		
+		// A.D d = new A.D();
+		a.method();			// 로컬 클래스 객체 생성을 위한 메서드 호출
 	}
 
-	static class C {
-		C() { System.out.println("C 객체가 생성됨"); }
-		int field1;
-		static int field2;
-		void method1() {}
-		static void method2() {}
-	}
+
 }
