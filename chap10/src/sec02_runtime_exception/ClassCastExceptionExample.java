cac49ef022e639b686cce60113b101b6a7ee3d86
ktyoung <ktyoung0507@gmail.com>
2022-07-27 10:07:32 +0900

    Try Catch

--- chap10/src/sec02_runtime_exception/ClassCastExceptionExample.java
+++ chap10/src/sec02_runtime_exception/ClassCastExceptionExample.java
@@ -7,11 +7,13 @@ public class ClassCastExceptionExample {
 		changeDog(dog);
 		
 		Cat cat = new Cat();
-		// changeDog(cat);
+		changeDog(cat);
 
 	}
 	public static void changeDog(Animal animal) {
-		Dog dog = (Dog) animal;	// ClassCastException 발생
+		Dog dog = (Dog) animal;		// 타입 변환(Casting)은 상위 클래스와 하위 클래스 사이에 발생하고, 구현 클래스와 인터페이스 사이에서도 발생한다.
+									// → 이러한 관계가 아니면 클래스는 다른 클래스로 타입 변환이 불가능하다.
+									// 따라서, 억지로 타입 변환을 시도하면 ClassCastException이 발생한다.
 	}
 
 }
--- chap10/src/sec02_runtime_exception/NumberFormatExceptionExample.java
+++ chap10/src/sec02_runtime_exception/NumberFormatExceptionExample.java
@@ -9,7 +9,8 @@ public class NumberFormatExceptionExample {
 		int value1 = Integer.parseInt(data1);	// 문자열로 되어있는 데이터를 숫자로 변환하는 Integer.parseInt(String s); 메서드
 		int value2 = Integer.parseInt(data2);	// → 문자열을 정수로 변환하여 반환
 												// Integer은 포장 클래스라 한다. 이 클래스의 parseXXX() 메서드는 문자열을 숫자로 변환할 수 있다.
-												// 이 메서드들은 매개값이 문자열이 숫자로 
+												// 이 메서드들은 매개값인 문자열이 숫자로 변환될 수 있다면 숫자를 리턴하지만,
+												// 숫자로 변환될 수 없는 문자가 포함되어 있다면 NumberFormatException 예외가 발생
 		int result = value1+ value2;
 		System.out.println(data1 + "+" + data2 + "+" + result);
 		
