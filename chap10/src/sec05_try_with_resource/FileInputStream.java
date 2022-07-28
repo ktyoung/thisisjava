d0f1a7493df377f408670c3e41aed34bd66b8748
ktyoung <ktyoung0507@gmail.com>
2022-07-28 09:56:31 +0900

    Try With Resource

--- chap10/src/sec05_try_with_resource/FileInputStream.java
+++ chap10/src/sec05_try_with_resource/FileInputStream.java
@@ -1,5 +1,7 @@
 package sec05_try_with_resource;
 
+// 리소스 객체는 AutoCloseable 인터페이스를 구현하고 있어야 한다.
+// AutoCloseable에는 close() 메서드가 정의되어 있고 try-with-resource는 이 close() 메서드를 자동 호출한다.
 public class FileInputStream implements AutoCloseable {
 	private String file;
 	
--- chap10/src/sec05_try_with_resource/TryWithResourceExample.java
+++ chap10/src/sec05_try_with_resource/TryWithResourceExample.java
@@ -3,6 +3,9 @@ package sec05_try_with_resource;
 public class TryWithResourceExample {
 
 	public static void main(String[] args) {
+		// try의 () 부분에 resource의 객체 구문이 들어간다.
+		// try 블록이 정상적으로 실행을 완료했거나, 예외가 발생하면 자동으로 AutoCloseable의 close() 메서드가 호출된다.
+		// try 블록에서 예외가 발생하면, 우선 close() 메서드로 리소스를 닫고 catch 블록을 실행한다.
 		try(FileInputStream fis = new FileInputStream("file.txt")) {
 			fis.read();
 			throw new Exception();
