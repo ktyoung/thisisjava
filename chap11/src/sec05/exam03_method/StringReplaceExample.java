6753f7bb7968603dfac83b49a82e07556aecbd6e
ktyoung <ktyoung0507@gmail.com>
2022-08-02 10:49:09 +0900

    replace()

---
+++ chap11/src/sec05/exam03_method/StringReplaceExample.java
@@ -0,0 +1,15 @@
+package sec05.exam03_method;
+
+// 첫 번째 매개값인 문자열을 찾아 두 번째 매개값인 문자열로 대치한 새로운 문자열을 생성하고 리턴한다.
+public class StringReplaceExample {
+
+	public static void main(String[] args) {
+		String oldStr = "자바는 객체지향언어입니다. 자바는 풍부한 API를 지원합니다.";
+		String newStr = oldStr.replace("자바", "JAVA");
+		
+		System.out.println(oldStr);
+		System.out.println(newStr);
+
+	}
+
+}
