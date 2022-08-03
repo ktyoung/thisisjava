eca9efbe8695c512efc9b7431f807c57971ebafa
ktyoung <ktyoung0507@gmail.com>
2022-08-03 10:55:51 +0900

    ArraySort()

--- chap11/src/sec09/exam01_array/Member.java
+++ chap11/src/sec09/exam01_array/Member.java
@@ -1,8 +1,19 @@
 package sec09.exam01_array;
 
-public class Member {
+public class Member implements Comparable<Member> {
 	String name;
 	Member(String name) {
 		this.name = name;
 	}
+	
+	// Comparable<Member> : Member 타입만 비교하기 위해 제네릭 <> 사용
+	// compareTo() : 비교값을 리턴하도록 오버라이딩
+	// 오름차순일 때, 자신이 매개값보다 낮을 경우 음수, 같을 경우 0, 높을 경우 양수를 리턴하면 된다.
+	// 내림차순일 때, 자신이 매개값보다 낮을 경우 양수, 같을 경우 0, 높을 경우 음수를 리턴하면 된다.
+	@Override
+	public int compareTo(Member o) {
+		return name.compareTo(o.name);	// name 필드값으로 정렬 → 유니코드를 비교해야 하므로, String의 compareTo() 리턴값을 사용
+	}
+	
+	
 }
---
+++ chap11/src/sec09/exam01_array/SortExample.java
@@ -0,0 +1,33 @@
+package sec09.exam01_array;
+
+import java.util.Arrays;
+
+// String 배열은 Arrays.sort() 메서드는 메서드의 매개값으로 지정해주면 자동으로 오름차순 정렬이 된다.
+// 사용자 정의 클래스 타입일 경우에는 클래스가 Comparable 인터페이스를 구현하고 있어야 정렬된다.
+public class SortExample {
+
+	public static void main(String[] args) {
+		int[] scores = { 99, 97, 98 };
+		Arrays.sort(scores);
+		for(int i=0; i<scores.length; i++){
+			System.out.println("scores[" + i + "] = " + scores[i]);
+		}
+		System.out.println();
+		
+		String[] names = { "홍길동", "박동수", "김민수" };
+		for(int i=0; i<names.length; i++) {
+			System.out.println("names[" + i + "] = " + names[i]);
+		}
+		System.out.println();
+		
+		Member m1 = new Member("홍길동");
+		Member m2 = new Member("박동수");
+		Member m3 = new Member("김민수");
+		Member[] members = { m1, m2, m3 };
+		Arrays.sort(members);
+		for(int i=0; i<members.length; i++) {
+			System.out.println("members[" + i + "] = " + members[i].name);
+		}
+	}
+
+}
