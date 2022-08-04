1c75890b82837f8135d6f35349841dc6e5501350
ktyoung <ktyoung0507@gmail.com>
2022-08-04 12:25:44 +0900

    java.time

--- chap11/src/sec14/exam01_java_time/DateTimeCompareExample.java
+++ chap11/src/sec14/exam01_java_time/DateTimeCompareExample.java
@@ -1,6 +1,7 @@
 package sec14.exam01_java_time;
 
 import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 
 public class DateTimeCompareExample {
 
@@ -18,7 +19,29 @@ public class DateTimeCompareExample {
 		} else if(startDateTime.isAfter(endDateTime)) {
 			System.out.println("종료했습니다." + "\n");
 		}
-
+		
+		System.out.println("[종료까지 남은 시간]");
+		long remainYear = startDateTime.until(endDateTime, ChronoUnit.YEARS);
+		long remainMonth = startDateTime.until(endDateTime, ChronoUnit.MONTHS);
+		long remainDay = startDateTime.until(endDateTime, ChronoUnit.DAYS);
+		long remainHour = startDateTime.until(endDateTime, ChronoUnit.HOURS);
+		long remainMinute = startDateTime.until(endDateTime, ChronoUnit.MINUTES);
+		long remainSecond = startDateTime.until(endDateTime, ChronoUnit.SECONDS);
+		
+		remainYear = ChronoUnit.YEARS.between(startDateTime, endDateTime);
+		remainMonth = ChronoUnit.MONTHS.between(startDateTime, endDateTime);
+		remainDay = ChronoUnit.DAYS.between(startDateTime, endDateTime);
+		remainHour = ChronoUnit.HOURS.between(startDateTime, endDateTime);
+		remainSecond = ChronoUnit.SECONDS.between(startDateTime, endDateTime);
+		
+		System.out.println("남은 해 : " + remainYear);
+		System.out.println("남은 달 : " + remainMonth);
+		System.out.println("남은 일 : " + remainDay);
+		System.out.println("남은 시간 : " + remainHour);
+		System.out.println("남은 분 : " + remainMinute);
+		System.out.println("남은 초 : " + remainSecond + "\n");
+		
+		
 	}
 
 }
