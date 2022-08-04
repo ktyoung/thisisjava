ff3c0532f59fb2fec6f65a3a923f613a2c0758a2
ktyoung <ktyoung0507@gmail.com>
2022-08-04 09:57:50 +0900

    Calendar

--- chap11/src/sec12/exam02_calendar/CalendarExample.java
+++ chap11/src/sec12/exam02_calendar/CalendarExample.java
@@ -47,6 +47,18 @@ public class CalendarExample {
 		} else {
 			strAmPm = "오후";
 		}
+		int hour = now.get(Calendar.HOUR);
+		int minute = now.get(Calendar.MINUTE);
+		int second = now.get(Calendar.SECOND);
+		
+		System.out.print(year + "년 ");
+		System.out.print(month + "월 ");
+		System.out.println(day + "일 ");
+		System.out.print(strWeek + "요일 ");
+		System.out.println(strAmPm + " ");
+		System.out.print(hour + "시 ");
+		System.out.print(minute + "분 ");
+		System.out.println(second + "초 ");
 	}
 
 }
