be4f640d3742dc6c718914be3f12725cea0b084a
ktyoung <ktyoung0507@gmail.com>
2022-08-04 12:01:11 +0900

    java.time

--- chap11/src/sec14/exam01_java_time/DateTimeChangeExample.java
+++ chap11/src/sec14/exam01_java_time/DateTimeChangeExample.java
@@ -1,5 +1,6 @@
 package sec14.exam01_java_time;
 
+import java.time.DayOfWeek;
 import java.time.LocalDateTime;
 import java.time.temporal.TemporalAdjusters;
 
@@ -20,12 +21,32 @@ public class DateTimeChangeExample {
 				.withSecond(20);
 		System.out.println("직접 변경 : " + targetDateTime);
 		
+		System.out.println();
+		
 		targetDateTime = now.with(TemporalAdjusters.firstDayOfYear());
 		System.out.println("이번 해의 첫 일 : " + targetDateTime);
 		targetDateTime = now.with(TemporalAdjusters.lastDayOfYear());
 		System.out.println("이번 해의 마지막 일 : " + targetDateTime);
 		targetDateTime = now.with(TemporalAdjusters.firstDayOfNextYear());
 		System.out.println("다음 해의 첫 일 : " + targetDateTime);
+		
+		System.out.println();
+		
+		targetDateTime = now.with(TemporalAdjusters.firstDayOfMonth());
+		System.out.println("이번 달의 첫 일 : " + targetDateTime);
+		targetDateTime = now.with(TemporalAdjusters.lastDayOfMonth());
+		System.out.println("이번 달의 마지막 일 : " + targetDateTime);
+		targetDateTime = now.with(TemporalAdjusters.firstDayOfNextMonth());
+		System.out.println("다음 달의 첫 일 : " + targetDateTime);
+		
+		System.out.println();
+		
+		targetDateTime = now.with(TemporalAdjusters.firstInMonth(DayOfWeek.MONDAY));
+		System.out.println("이번 달의 첫 월요일 : " + targetDateTime);
+		targetDateTime = now.with(TemporalAdjusters.next(DayOfWeek.MONDAY));
+		System.out.println("돌아오는 월요일 : " + targetDateTime);
+		targetDateTime = now.with(TemporalAdjusters.previous(DayOfWeek.MONDAY));
+		System.out.println("지난 월요일 : " + targetDateTime);
 	}
 
 }
