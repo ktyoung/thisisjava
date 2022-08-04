cc2e5ad746b5027054b45915e6a435daf72305da
ktyoung <ktyoung0507@gmail.com>
2022-08-04 09:44:24 +0900

    Date()

--- chap11/src/sec12/exam01_date/DateExample.java
+++ chap11/src/sec12/exam01_date/DateExample.java
@@ -10,7 +10,7 @@ public class DateExample {
 		String strNow1 = now.toString();	// 현재 날짜를 문자열로 얻고 싶으면 toString() 메서드 사용
 		System.out.println(strNow1);
 		
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyy년 MM월 dd시 mm분 ss초");
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy년 MM월 dd일 hh시 mm분 ss초");
 		String strNow2 = sdf.format(now);	// 특정 문자열 포멧으로 얻고 싶다면 SimpleDataFormat 클래스 사용
 		System.out.println(strNow2);
 
