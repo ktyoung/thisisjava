2b140ef48e20e9473b2640390d18ba3ca6545dd7
ktyoung <ktyoung0507@gmail.com>
2022-08-04 09:42:39 +0900

    Date()

---
+++ chap11/src/sec12/exam01_date/DateExample.java
@@ -0,0 +1,19 @@
+package sec12.exam01_date;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DateExample {
+
+	public static void main(String[] args) {
+		Date now = new Date();
+		String strNow1 = now.toString();
+		System.out.println(strNow1);
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy년 MM월 dd시 mm분 ss초");
+		String strNow2 = sdf.format(now);
+		System.out.println(strNow2);
+
+	}
+
+}
