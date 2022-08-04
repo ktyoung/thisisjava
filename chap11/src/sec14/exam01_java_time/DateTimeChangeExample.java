5eb70c609c96af5597f13951b1fe775086b0742f
ktyoung <ktyoung0507@gmail.com>
2022-08-04 11:56:08 +0900

    java.time

--- chap11/src/sec14/exam01_java_time/DateTimeChangeExample.java
+++ chap11/src/sec14/exam01_java_time/DateTimeChangeExample.java
@@ -1,6 +1,7 @@
 package sec14.exam01_java_time;
 
 import java.time.LocalDateTime;
+import java.time.temporal.TemporalAdjusters;
 
 public class DateTimeChangeExample {
 
@@ -18,6 +19,13 @@ public class DateTimeChangeExample {
 				.withMinute(30)
 				.withSecond(20);
 		System.out.println("직접 변경 : " + targetDateTime);
+		
+		targetDateTime = now.with(TemporalAdjusters.firstDayOfYear());
+		System.out.println("이번 해의 첫 일 : " + targetDateTime);
+		targetDateTime = now.with(TemporalAdjusters.lastDayOfYear());
+		System.out.println("이번 해의 마지막 일 : " + targetDateTime);
+		targetDateTime = now.with(TemporalAdjusters.firstDayOfNextYear());
+		System.out.println("다음 해의 첫 일 : " + targetDateTime);
 	}
 
 }
