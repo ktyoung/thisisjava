a2df2e545d937906971a09c9c3097a41572184d6
ktyoung <ktyoung0507@gmail.com>
2022-08-04 10:03:06 +0900

    TimeZone

---
+++ chap11/src/sec12/exam02_calendar/PrintTimeZoneID.java
@@ -0,0 +1,15 @@
+package sec12.exam02_calendar;
+
+import java.util.TimeZone;
+
+public class PrintTimeZoneID {
+
+	public static void main(String[] args) {
+		String[] availableIDs = TimeZone.getAvailableIDs();
+		for(String id : availableIDs) {
+			System.out.println(id);
+		}
+	}
+
+}
+   
