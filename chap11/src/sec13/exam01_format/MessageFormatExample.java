955b14d1a9a3c99e95a957f4b0f51068ad120ba8
ktyoung <ktyoung0507@gmail.com>
2022-08-04 10:47:11 +0900

    MessageFormat

---
+++ chap11/src/sec13/exam01_format/MessageFormatExample.java
@@ -0,0 +1,24 @@
+package sec13.exam01_format;
+
+import java.text.MessageFormat;
+
+public class MessageFormatExample {
+
+	public static void main(String[] args) {
+		String id = "java";
+		String name = "홍길동";
+		String tel = "010-123-5678";
+		
+		String text = "회원 ID : {0} \n회원이름 : {1} \n회원전화 : {2}";
+		String result1 = MessageFormat.format(text, id, name, tel);
+		System.out.println(result1);
+		System.out.println();
+		
+		String sql = "insert into member value( {0},{1},{2} )";
+		Object[] arguments = {"java","홍길동","010-123-5678"};
+		String result2 = MessageFormat.format(sql, arguments);
+		System.out.println(result2);
+
+	}
+
+}
