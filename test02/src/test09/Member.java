da956de08bfbf0266df04ce4c3145e91b031a9c4
ktyoung <ktyoung0507@gmail.com>
2022-08-05 10:37:23 +0900

    test02

--- test02/src/test09/Member.java
+++ test02/src/test09/Member.java
@@ -9,6 +9,7 @@ public class Member {
     	this.name = name;
 	}
     
+	// "toString()" 메서드는 객체가 가지고 있는 정보나 값들을 문자열로 만들어 리턴하는 메서드
     @Override
     public String toString() {
     	return id + " : " + name;
