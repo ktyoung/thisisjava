f2571559e6a2b25aa017b36e5cf2306acdf46add
ktyoung <ktyoung0507@gmail.com>
2022-08-05 10:21:04 +0900

    test02

--- test02/src/test06/PatternMatcherExample.java
+++ test02/src/test06/PatternMatcherExample.java
@@ -7,8 +7,7 @@ public class PatternMatcherExample {
     public static void main(String[] args) {
         String id = "5Angel1004";
         // [] : 문서의 집합 범위, {} : 횟수 또는 범위
-        // []
-        String regExp = "[a-zA-Z][a-zA-Z0-9]{7,11}";	// → 앞 한 글자를 뺀 두 번
+        String regExp = "[a-zA-Z][a-zA-Z0-9]{7,11}";
         boolean isMatch = Pattern.matches(regExp, id);
         if (isMatch) {
             System.out.println("ID로 사용할 수 있습니다.");
