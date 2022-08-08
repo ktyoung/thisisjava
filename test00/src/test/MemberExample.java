37772ce307af88b5536285a0327f5df7bd24a79c
ktyoung <ktyoung0507@gmail.com>
2022-08-08 10:29:34 +0900

    test02

---
+++ test00/src/test/MemberExample.java
@@ -0,0 +1,11 @@
+package test;
+
+public class MemberExample {
+	public static void main(String[] args) {
+		String strData1 = "200";
+		int intData1 = Integer.parseInt(strData1);
+		
+		int intData2 = 150;
+		String strData2 = String.valueOf(intData2);
+	}
+}
--- test02/src/test04/BytesToStringExample.java
+++ test02/src/test04/BytesToStringExample.java
@@ -1,10 +1,15 @@
 package test04;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class BytesToStringExample {
 	 
     public static void main(String[] args) {
-        byte[] bytes = {73, 32, 108, 111, 118, 101, 32, 121, 111, 117};
-        String str = new String(bytes);
-        System.out.println(str);
+    	Date now = new Date();
+
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy년 MM월 dd일 E요일 HH시 mm분");
+
+		System.out.println(sdf.format(now));
     }
 }
