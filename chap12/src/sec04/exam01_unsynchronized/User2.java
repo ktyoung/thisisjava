6d93ba6ce0d7e8542d3147fff74dbee4877c6f3c
ktyoung <ktyoung0507@gmail.com>
2022-08-17 13:05:59 +0900

    Unsynchronized

--- chap12/src/sec04/exam01_unsynchronized/User2.java
+++ chap12/src/sec04/exam01_unsynchronized/User2.java
@@ -1,14 +1,14 @@
 package sec04.exam01_unsynchronized;
 
-public class User1 extends Thread {
+public class User2 extends Thread {
 	private Calculator calculator;
 	
 	public void setCalculator(Calculator calculator) {
-		this.setName("User1");
+		this.setName("User2");
 		this.calculator = calculator;
 	}
 	
 	public void run() {
-		calculator.setMemory(100);
+		calculator.setMemory(50);
 	}
 }
