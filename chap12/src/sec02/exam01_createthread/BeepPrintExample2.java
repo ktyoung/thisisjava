5baf63dc43227919d9fe81a6da9fd21e7670128d
ktyoung <ktyoung0507@gmail.com>
2022-08-17 12:02:12 +0900

    BeepPrintExample

--- chap12/src/sec02/exam01_createthread/BeepPrintExample2.java
+++ chap12/src/sec02/exam01_createthread/BeepPrintExample2.java
@@ -5,9 +5,25 @@ import java.awt.Toolkit;
 public class BeepPrintExample2 {
 
 	public static void main(String[] args) {
-		
+		// 规过 1
+		/*
 		Runnable beepTask = new BeepTask();
 		Thread thread = new Thread(beepTask);
+		*/
+		
+		// 规过 2
+		Thread thread = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				Toolkit toolkit = Toolkit.getDefaultToolkit();
+				for(int i=0; i<5; i++) {
+					toolkit.beep();
+					try {
+						Thread.sleep(500);
+					} catch (Exception e) {}
+				}
+			}
+		});
 		
 		thread.start();
 		
